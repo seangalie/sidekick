ad2f4f0170b00d6f3afafa6536120f7a087697f2
Sean Galie <2238841+seangalie@users.noreply.github.com>
2026-03-03 07:18:21 -0500

    Add pre-check placeholders and menu skeleton
    Introduce several placeholder pre-check functions. Replace the menu placeholder with a gum-styled interactive menu template.

--- sidekick.sh
+++ sidekick.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # +---------------------------------------------------------------------------+
 # |                                 Sidekick                                  |
@@ -13,34 +13,105 @@
 
 set -euo pipefail
 
-menu_main() {
-    echo "I'm a placeholder for a main menu."
+check_connection() {
+    echo "I'm a placeholder for a connection check."
     read -p "Press [Enter] to continue..."
+}
+
+check_sudo() {
+    echo "I'm a placeholder for a root/superuser permissions check."
+    read -p "Press [Enter] to continue..."
+}
+
+check_version() {
+    echo "I'm a placeholder for a Debian version check."
+    read -p "Press [Enter] to continue..."
+}
+
+check_deps() {
+    echo "I'm a placeholder for a script dependencies check."
+    read -p "Press [Enter] to continue..."
+}
+
+check_gum() {
+    echo "I'm a placeholder for a check that gum is installed, and to install it if it isn't."
+    read -p "Press [Enter] to continue..."
+}
+
+check_fetch() {
+    echo "I'm a placeholder for a check that the fetch utility is installed, and to install it if it isn't."
+    read -p "Press [Enter] to continue..."
+}
+
+menu_main() {
+    gum style --foreground 57 --padding "1 1" "Choose security practices or actions to implement:"
+    readarray -t ENV_OPTIONS < <(gum choose --no-limit \
+        "Initial Setup for a new Debian installation" \
+        "Assist in upgrading from Debian 12 to Debian 13" \
+        "Configure security options for this environment" \
+        "Install useful shell prompt upgrades" \
+        "Update installed packages")
+    for OPTION in "${ENV_OPTIONS[@]}"; do
+        case $OPTION in
+            "Initial Setup for a new Debian installation")
+                sidekick_setup
+                ;;
+            "Assist in upgrading from Debian 12 to Debian 13")
+                sidekick_upgrade
+                ;;
+            "Configure security options for this environment")
+                sidekick_secure
+                ;;
+            "Install useful shell prompt upgrades")
+                sidekick_prompt
+                ;;
+            "Update installed packages")
+                sidekick_update
+                ;;
+
+            *)
+                gum style --foreground 57 --padding "1 1" "Nothing selected..."
+                sleep 1
+                ;;
+        esac
+    done
+
 }
 
 sidekick_setup() {
     echo "I'm a placeholder for the initial setup script."
     read -p "Press [Enter] to continue..."
-}
-
-sidekick_update() {
-    echo "I'm a placeholder for the regular update script."
-    read -p "Press [Enter] to continue..."
+    menu_main
 }
 
 sidekick_upgrade() {
     echo "I'm a placeholder for the version upgrade script."
     read -p "Press [Enter] to continue..."
+    menu_main
 }
 
 sidekick_secure() {
     echo "I'm a placeholder for the security script."
     read -p "Press [Enter] to continue..."
+    menu_main
 }
 
 sidekick_prompt() {
     echo "I'm a placeholder for the local starship prompt config."
     read -p "Press [Enter] to continue..."
+    menu_main
 }
 
+sidekick_update() {
+    echo "I'm a placeholder for the regular update script."
+    read -p "Press [Enter] to continue..."
+    menu_main
+}
+
+check_connection
+check_sudo
+check_version
+check_deps
+check_gum
+check_fetch
 menu_main
