918aec064a3144cc79731d18c6f16701af0a34ac
Sean Galie <2238841+seangalie@users.noreply.github.com>
2026-03-03 05:51:54 -0500

    Add placeholder functions
    Add placeholder functions for main menu, setup, update, upgrade, security and prompt configuration.

--- sidekick.sh
+++ sidekick.sh
@@ -2,7 +2,7 @@
 #
 # +---------------------------------------------------------------------------+
 # |                                 Sidekick                                  |
-# |  A configuration script and server management tool for Debian instances.  |
+# |    A configuration script and management tool for Debian environments     |
 # +---------------------------------------------------------------------------+
 #
 # This script is designed to help configure and manage Debian 12 (Bookworm) and
@@ -10,3 +10,37 @@
 #
 # Copyright 2026, Sean Galie
 # SPDX-License-Identifier: Apache-2.0
+
+set -euo pipefail
+
+menu_main() {
+    echo "I'm a placeholder for a main menu."
+    read -p "Press [Enter] to continue..."
+}
+
+sidekick_setup() {
+    echo "I'm a placeholder for the initial setup script."
+    read -p "Press [Enter] to continue..."
+}
+
+sidekick_update() {
+    echo "I'm a placeholder for the regular update script."
+    read -p "Press [Enter] to continue..."
+}
+
+sidekick_upgrade() {
+    echo "I'm a placeholder for the version upgrade script."
+    read -p "Press [Enter] to continue..."
+}
+
+sidekick_secure() {
+    echo "I'm a placeholder for the security script."
+    read -p "Press [Enter] to continue..."
+}
+
+sidekick_prompt() {
+    echo "I'm a placeholder for the local starship prompt config."
+    read -p "Press [Enter] to continue..."
+}
+
+menu_main
