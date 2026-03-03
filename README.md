92a90c6fc1525a500c2f18e38ae929674bc98504
Sean Galie <2238841+seangalie@users.noreply.github.com>
2026-03-03 05:39:33 -0500

    Initial Commit
    Add initial project files: Apache License 2.0 (LICENSE), README, and a script template.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sidekick
+
+A configuration script and server management tool for Debian instances.
+
+## License
+
+Licensed under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
---
+++ sidekick.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# +---------------------------------------------------------------------------+
+# |                                 Sidekick                                  |
+# |  A configuration script and server management tool for Debian instances.  |
+# +---------------------------------------------------------------------------+
+#
+# This script is designed to help configure and manage Debian 12 (Bookworm) and
+# Debian 13 (Trixie) remote servers, local workstations, and virtual machines.
+#
+# Copyright 2026, Sean Galie
+# SPDX-License-Identifier: Apache-2.0
